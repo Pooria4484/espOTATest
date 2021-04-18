ba6c061122fe8a97f6ee7df9979151625acdb2c5
Pooria4484 <47162303+Pooria4484@users.noreply.github.com>
2021-04-19 02:23:34 +0430

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+import sdist_upip
+
+setup(name='upiptest',
+      version='0.0.2',
+      description='Minimalistic file shell using native Python syntax.',
+      long_description='Minimalistic file shell using native Python syntax.',
+      url='https://github.com/Pooria4484/espOTATest/app',
+      author='pooria-lib Developers',
+      author_email='info@msb-co.ir',
+      maintainer='pooria-lib Developers',
+      maintainer_email='info@msb-co.ir',
+      license='/MSB',
+      cmdclass={'sdist': sdist_upip.sdist},
+      py_modules=['upiptest'])
